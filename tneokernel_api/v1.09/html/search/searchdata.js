27f7eea32d8f3935d0baf5bf6592f314a921628d
Dmitry Frank <mail@dmitryfrank.com>
2024-12-28 20:20:54 +0200

    docs updated: v1.09

---
+++ tneokernel_api/v1.09/html/search/searchdata.js
@@ -0,0 +1,42 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghilmnopqstuwy",
+  1: "_t",
+  2: "t",
+  3: "_ty",
+  4: "abcdefghilmnopstuw",
+  5: "ct",
+  6: "t",
+  7: "_t",
+  8: "_amnt",
+  9: "abcdfilpqtuw"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues",
+  8: "defines",
+  9: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator",
+  8: "Macros",
+  9: "Pages"
+};
+
